04eef2b73a97e987e85a7325f3c003be4a5f58c8
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-21 01:07:22 +0330

    Add Migration in DataLayer

--- DataLayer/Context/UPCodeContext.cs
+++ DataLayer/Context/UPCodeContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace DataLayer.Context
 {
-    internal class UPCodeContext
+    public class UPCodeContext:DbContext
     {
+        public UPCodeContext(DbContextOptions<UPCodeContext> options)
+            :base(options)
+        {
+
+        }
+
+        #region USER
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserGender> UserGenders { get; set; }
+
+        #endregion
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+
+
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
