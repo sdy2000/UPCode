0e9744c47210f5c8daa59453420cf021cc6ff9eb
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-21 01:03:13 +0330

    In DataLayer => Acc Context Folder and create UPCodeContext
    InWeb =>
    Add ConnectionString in appsettings,
    Add dbContext in Program
    
    Create refrence between Web<=Core,DataLayer||and Core<=DataLayer

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,17 @@
+using DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region DATA BASE CONTEXT UPCODE
+
+builder.Services.AddDbContext<UPCodeContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UPCodeConnection"));
+});
+
+#endregion
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
