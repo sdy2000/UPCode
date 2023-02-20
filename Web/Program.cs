e0d68776f712d9c9cd46cdc20e1dec8b9e53869b
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-21 01:18:34 +0330

    Update Program.cs (add Map ControllerRoute)

--- Web/Program.cs
+++ Web/Program.cs
@@ -12,8 +12,19 @@ builder.Services.AddDbContext<UPCodeContext>(options =>
 
 #endregion
 
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); ;
 
 app.Run();
