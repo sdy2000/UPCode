97eaa13ee96f33aa91485e388452b446969fea0f
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-21 00:49:43 +0330

    Add Entities Folder And Add Role, User, UserRole,UserGender Table in User Folder

---
+++ DataLayer/Entities/User/User.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Entities.User
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+
+        [Display(Name = "جنسیت")]
+        public int? GenderId { get; set; }
+
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "نام")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string? FirstName { get; set; }
+
+
+        [Display(Name = "نام خانوادگی")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string? LastName { get; set; }
+
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "شماره موبایل")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string? PhonNumber { get; set; }
+
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "تصویر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        public string UserAvatar { get; set; }
+
+
+        [Display(Name = "کد فعال سازی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        public string ActiveCode { get; set; }
+
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+
+
+        [Display(Name = "تاریخ ثبت نام")]
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+
+        [Display(Name = "حذف شده است؟")]
+        public bool IsDelete { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+        #region RELATIONS
+
+        [ForeignKey("GenderId")]
+        public UserGender UserGender { get; set; }
+        public List<UserRole> UserRoles { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ DataLayer/Entities/User/UserGender.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataLayer.Entities.User
+{
+    public class UserGender
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int GenderId { get; set; }
+
+
+        [Display(Name = "جنسیت")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string GenderTitle { get; set; }
+
+
+
+        #region RELATION
+
+        public List<User> Users { get; set; }
+
+        #endregion
+
+    }
+}
