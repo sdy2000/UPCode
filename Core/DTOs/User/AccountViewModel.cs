71c170da94f8d44e17b13d2772ab20f2f625e2c3
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-23 04:28:12 +0330

    In Web => Add Account Controller and inside it add Register action with view
    In Core =>
    Add DTOs Folder <= User <= AccountViewModel.cs and inside it add RegisterViewModel class

---
+++ Core/DTOs/User/AccountViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTOs
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        [MaxLength(20, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد!")]
+        [MinLength(6, ErrorMessage = "{0} نمیتواند کمتر از {1} باشد!")]
+        public string Password { get; set; }
+
+
+        [Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند!")]
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        public string RePassword { get; set; }
+
+
+        [Display(Name = "قوانین و مقررات")]
+        [Required(ErrorMessage = "لطفا {0} را تایید نمایید.")]
+        public bool Rules { get; set; }
+    }
+}
---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
