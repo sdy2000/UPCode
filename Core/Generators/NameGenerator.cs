efd49aaeb893895f8c88b486e1314bb3d31140a1
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-23 04:58:48 +0330

    In Core => Add Generators <= NameGenerator class Add Security <= PasswordHelper
    In Web =>
    Add Post method for Register view in AccountController

---
+++ Core/Generators/NameGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Generators
+{
+    public class NameGenerator
+    {
+        public static string GeneratorUniqCode()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+    }
+}
--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Core.DTOs;
+﻿using Core.Convertors;
+using Core.DTOs;
+using Core.Generators;
+using Core.Security;
+using Core.Servises.Interfaces;
 using DataLayer.Entities.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +10,12 @@ namespace Web.Controllers
 {
     public class AccountController : Controller
     {
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
 
 
 
@@ -18,6 +28,47 @@ namespace Web.Controllers
         }
 
 
+
+        [Route("Register")]
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid || register.Password != register.RePassword)
+                return View(register);
+
+            if (_userService.IsExistUserName(register.UserName))
+            {
+                ModelState.AddModelError("UserName", "نام کاربری تکراری میباشد !");
+                return View(register);
+            }
+            if (_userService.IsExistEmail(register.Email))
+            {
+                ModelState.AddModelError("Email", "ایمیل وارد شده تکراری میباشد !");
+            }
+
+            var user = new User()
+            {
+                UserName = register.UserName,
+                Email = FixedText.FixedEmail(register.Email),
+                ActiveCode = NameGenerator.GeneratorUniqCode(),
+                IsActive = false,
+                Password = PasswordHelper.EncodePasswordMd5(register.Password),
+                UserAvatar = "No-Photo.jpg"
+            };
+
+            // TODO : Add User
+
+
+            #region SEND ACTIVATION EMAIL
+
+            // TODO : Send Activation Email
+
+            #endregion
+
+
+            // TODO Add _SuccessRegister View
+            return View("_SuccessRegister", user);
+        }
 
         #endregion
     }
