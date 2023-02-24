f77038d6009b7c4e84e2cf7739fe7655ff5da3fd
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-25 01:38:54 +0330

    Add Email Body Generator in Generators folder

---
+++ Core/Generators/EmailBodyGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Generators
+{
+    public class EmailBodyGenerator
+    {
+        public static string SendActiveEmail(string userName, string activeCode)
+        {
+
+            string body = @"<div style=""direction:rtl;padding20px"">
+                        <h2>UserName Hi !</h2>
+                        <span>DateTime</span>
+                        <p>Tanks for register in <a href=""#"">UPCode</a> , You must activate your account to continue .</p>
+                        <h3 style=""color:forestgreen;""><a href=""http://localhost:3000/success-activation/ActiveCode"">Account activation !</a></h3>
+                        </div>";
+            body = body.Replace("UserName", userName);
+            body = body.Replace("ActiveCode", activeCode);
+            body = body.Replace("DateTime", DateTime.Now.ToString());
+
+            return body;
+        }
+    }
+}
