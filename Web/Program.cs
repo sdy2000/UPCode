0f79915b5644fc376f5ab0b6cda57e915b3df66e
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-02-26 04:53:30 +0330

    Update Program.cs for Authentication

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,9 +1,26 @@
 using Core.Servises;
 using Core.Servises.Interfaces;
 using DataLayer.Context;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region AUTHENTICATION
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(options =>
+{
+    options.LoginPath = "/Login";
+    options.LogoutPath = "/Logout";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(43200);
+});
+
+#endregion
 
 #region DATA BASE CONTEXT UPCODE
 
@@ -13,7 +30,6 @@ builder.Services.AddDbContext<UPCodeContext>(options =>
 });
 
 #endregion
-
 
 #region IoC
 
@@ -30,7 +46,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
+app.UseAuthentication();
 app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
