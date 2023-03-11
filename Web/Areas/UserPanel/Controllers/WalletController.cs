6d95ff5a141b029456d5a63e04600701390e0fd9
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-12 01:54:37 +0330

    Add WalletController and create Wallet View

---
+++ Web/Areas/UserPanel/Controllers/WalletController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Servises.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    [Authorize]
+    public class WalletController : Controller
+    {
+        private IUserService _userService;
+        public WalletController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [Route("UserPanel/Wallet")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
