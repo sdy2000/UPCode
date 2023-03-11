5711fd40e7d5c8a4e3748d5032e2adeadabd1703
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-12 01:31:33 +0330

    Add WalletType and Wallet Table

---
+++ DataLayer/Entities/Wallets/Wallet.cs
@@ -0,0 +1,53 @@
+﻿using DataLayer.Entities.User;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Datalayer.Entities.Wallets
+{
+    public class Wallet
+    {
+        [Key]
+        public int WalletId { get; set; }
+
+
+        [Display(Name = "Transaction type")]
+        [Required(ErrorMessage = "Please enter {0}.")]
+        public int TypeId { get; set; }
+
+
+        [Display(Name = "UserId")]
+        [Required(ErrorMessage = "Please enter {0}.")]
+        public int UserId { get; set; }
+
+
+        [Display(Name = "Abount")]
+        [Required(ErrorMessage = "Please enter {0}.")]
+        public int Amount { get; set; }
+
+
+        [Display(Name = "Description")]
+        [MaxLength(300)]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Is Pay")]
+        public bool IsPay { get; set; }
+
+
+        [Display(Name = "Date Time")]
+        public DateTime CreateDate { get; set; }
+
+
+
+
+
+        #region RELATION
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [ForeignKey("TypeId")]
+        public WalletType WalletType { get; set; }
+
+        #endregion
+    }
+}
---
+++ DataLayer/Entities/Wallets/WalletType.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Datalayer.Entities.Wallets
+{
+    public class WalletType
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TypeId { get; set; }
+
+
+        [Required]
+        [MaxLength(100)]
+        public string TypeTitle { get; set; }
+
+
+
+
+        #region RELATIONS
+
+        public List<Wallet> Wallets { get; set; }
+
+        #endregion
+    }
+}
