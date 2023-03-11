522aaf99675da54cc75ddb40fafb0a6f3cddd68d
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-12 02:30:37 +0330

    Update Wallet Post method, Create OnlinePayment in HomeController And Created GetWalletByWalletId method

--- Web/Areas/UserPanel/Controllers/WalletController.cs
+++ Web/Areas/UserPanel/Controllers/WalletController.cs
@@ -35,6 +35,16 @@ namespace Web.Areas.UserPanel.Controllers
             }
             int walletId = _userService.ChargeWallet(User.Identity.Name, charge.Amount, "Charge account");
 
+            #region ONLINE PAYMENT
+
+            var payment = new ZarinpalSandbox.Payment(charge.Amount);
+            var res = payment.PaymentRequest("Charge Wallet", "https://localhost:5074/OnlinePayment/" + walletId);
+            if (res.Result.Status == 100)
+            {
+                return Redirect("https://sandbox.zarinpal.com/pg/StartPay/" + res.Result.Authority);
+            }
+
+            #endregion
 
             ViewBag.ListWallet = _userService.GetWalletUser(User.Identity.Name);
             return View();
