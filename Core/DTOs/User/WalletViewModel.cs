e0f4f96e5a97fb90f5dd7ddfc8f2812a52afc5aa
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-12 02:04:06 +0330

    Add WalletViewModel and Update Wallet Veiw

---
+++ Core/DTOs/User/WalletViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTOs
+{
+    public class ChargeWalletViewModel
+    {
+        [Display(Name = "Abount")]
+        [Required(ErrorMessage = "Please enter {0}.")]
+        public int Amount { get; set; }
+    }
+
+    public class WalletViewModel
+    {
+        public int Amount { get; set; }
+        public int Type{ get; set; }
+        public bool IsPay { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
