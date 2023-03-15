c3ccd8bb44778f4cc895827dc066b6a9f15a4d95
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-16 02:37:16 +0330

    Create Role Index And Add Get method for this

---
+++ Web/Pages/Admin/Role/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Core.Servises.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Admin.Role
+{
+    public class IndexModel : PageModel
+    {
+        private IPermissionService _permission;
+        public IndexModel(IPermissionService permission)
+        {
+            _permission = permission;
+        }
+
+        public List<DataLayer.Entities.User.Role> RoleList { get; set; }
+        public void OnGet(string userRoleFilter)
+        {
+            RoleList = _permission.GetAllRoles(userRoleFilter);
+        }
+    }
+}
