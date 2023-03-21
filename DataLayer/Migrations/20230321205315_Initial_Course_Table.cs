2f1d09c2d69a5ded2895d2577a5da04e2198ca4a
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-22 01:24:08 +0430

    Add migration For Course Tables

---
+++ DataLayer/Migrations/20230321205315_Initial_Course_Table.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Course_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_WalletTypes_TypeId",
+                table: "Wallets");
+
+            migrationBuilder.CreateTable(
+                name: "CourseGroups",
+                columns: table => new
+                {
+                    GroupId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseGroups", x => x.GroupId);
+                    table.ForeignKey(
+                        name: "FK_CourseGroups_CourseGroups_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "CourseGroups",
+                        principalColumn: "GroupId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseLevels",
+                columns: table => new
+                {
+                    LevelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LevelTitle = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseLevels", x => x.LevelId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseStatuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseStatuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    SubGroupId = table.Column<int>(type: "int", nullable: true),
+                    TeacherId = table.Column<int>(type: "int", nullable: false),
+                    LevelId = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    CourseTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CourseDescription = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CoursePrice = table.Column<int>(type: "int", nullable: false),
+                    Tags = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    CourseImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseDemoFileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseGroups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "CourseGroups",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseGroups_SubGroupId",
+                        column: x => x.SubGroupId,
+                        principalTable: "CourseGroups",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseLevels_LevelId",
+                        column: x => x.LevelId,
+                        principalTable: "CourseLevels",
+                        principalColumn: "LevelId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseStatuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "CourseStatuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Users_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseEpisodes",
+                columns: table => new
+                {
+                    EpisodeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    EpisodeTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EpisodFileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFree = table.Column<bool>(type: "bit", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EpisodeTime = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseEpisodes", x => x.EpisodeId);
+                    table.ForeignKey(
+                        name: "FK_CourseEpisodes_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseEpisodes_CourseId",
+                table: "CourseEpisodes",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseGroups_ParentId",
+                table: "CourseGroups",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_GroupId",
+                table: "Courses",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_LevelId",
+                table: "Courses",
+                column: "LevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_StatusId",
+                table: "Courses",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_SubGroupId",
+                table: "Courses",
+                column: "SubGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_TeacherId",
+                table: "Courses",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId",
+                principalTable: "Permissions",
+                principalColumn: "PermissionId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_WalletTypes_TypeId",
+                table: "Wallets",
+                column: "TypeId",
+                principalTable: "WalletTypes",
+                principalColumn: "TypeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_WalletTypes_TypeId",
+                table: "Wallets");
+
+            migrationBuilder.DropTable(
+                name: "CourseEpisodes");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "CourseGroups");
+
+            migrationBuilder.DropTable(
+                name: "CourseLevels");
+
+            migrationBuilder.DropTable(
+                name: "CourseStatuses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId",
+                principalTable: "Permissions",
+                principalColumn: "PermissionId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_WalletTypes_TypeId",
+                table: "Wallets",
+                column: "TypeId",
+                principalTable: "WalletTypes",
+                principalColumn: "TypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
