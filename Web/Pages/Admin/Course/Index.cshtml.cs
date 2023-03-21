10ac8e9aeff1182e8c130079e9c6e05393b770b2
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-22 01:51:30 +0430

    Create Index page Veiw and Update Admin Latout

--- Web/Pages/Admin/Course/Index.cshtml.cs
+++ Web/Pages/Admin/Course/Index.cshtml.cs
@@ -1,10 +1,12 @@
 using Core.DTOs;
+using Core.Security;
 using Core.Servises.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages.Admin.Course
 {
+    [PermissionChecker(13)]
     public class IndexModel : PageModel
     {
         private ICourseService _courseService;
