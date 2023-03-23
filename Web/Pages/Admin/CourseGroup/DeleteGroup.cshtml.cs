2dc7a69abb4a2aa73ad4765f926ad319acabc747
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-23 05:35:05 +0430

    Create DeleteGroup page and Initial Get method

---
+++ Web/Pages/Admin/CourseGroup/DeleteGroup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Admin.CourseGroup
+{
+    public class DeleteGroupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
